90b88eba2c52ac33cf1d1a40fbde01651043d3ef
simon <simonrbrain@yahoo.co.uk>
2025-01-28 18:56:38 +0000

    added Interval function

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const searchWeather = () => {
 			city.querySelector('img').src= 'https://flagsapi.com/'+data.sys.country+'/shiny/32.png';
 			temprature.querySelector('img').src= 'https://openweathermap.org/img/wn/'+data.weather[0].icon+'@4x.png';
 			temprature.querySelector('figcaption span').innerText = Math.round(data.main.temp);
-			feelLike.innerHTML = 'Feels like ' +Math.round(data.main.feels_like)+'<sup>o</sup>C<br>Min '+Math.round(data.main.temp_min)+'<sup>o</sup>C Max '+Math.round(data.main.temp_max)+'<sup>o</sup>C';
+			feelLike.innerHTML = 'Feels like ' +Math.round(data.main.feels_like)+'<sup> o</sup>C<br>Min '+Math.round(data.main.temp_min)+'<sup> o</sup>C Max '+Math.round(data.main.temp_max)+'<sup> o</sup>C';
 			desc.innerText = data.weather[0].description;
 			cloud.innerText = data.clouds.all;
 			rain.innerText = data.main.humidity;
